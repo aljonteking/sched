385c2464b1f69d842cfece8ec53f6821084894ac
aljonteking <aljonteking@gmail.com>
2024-12-20 09:47:11 +0800

    Delete sectionload directory

--- sectionload/schedule/urls.py
+++
@@ -1,7 +0,0 @@
-# sectionload/schedule/urls.py
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-]
--- sectionload/schedule/views.py
+++
@@ -1,5 +0,0 @@
-# sectionload/schedule/views.py
-from django.shortcuts import render
-
-def home(request):
-    return render(request, 'schedule/index.html')
